c1853f9c7fc560d23bed5c502de15cc9b72678b2
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-14 20:11:43 +0300

    add constructors

--- Lab1/Rectangle.h
+++ Lab1/Rectangle.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <cstdint>
 class Rectangle
 {
+	struct point { int32_t x; int32_t y; };
+	point upper_left_corner;
+	point lower_right_corner;
+public:
+	Rectangle() 
+	{
+		upper_left_corner = { 0, 0 };  
+		lower_right_corner = { 0, 0 };
+	}
+	Rectangle(const point point_1, const point point_2) : Rectangle()
+	{
+		upper_left_corner = point_1;
+		lower_right_corner = point_2;
+	}
+	Rectangle(const Rectangle& rectangle)
+	{
+		upper_left_corner = rectangle.upper_left_corner;
+		lower_right_corner = rectangle.lower_right_corner;
+	}
+
 };
 
